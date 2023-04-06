350509622569745f27d6c02cf646495de26b6ccd
Shashwatt000 <sharmashashwat826@gmail.com>
2023-04-06 08:42:10 -0700

    Alter commands

--- alter.sql
+++ alter.sql
@@ -1,1 +1,2 @@
 alter table courses change column CourseName Course varchar(255);
+alter table students add CourseID int;
---
+++ select.sql
@@ -0,0 +1,2 @@
+    select courses.Course , students.course from courses inner join courses on students.CourseID=courses.CourseID;
+    
